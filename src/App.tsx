b1545c09c8fa39ea6941efa7b06013598b134a60
Gabriela França <gabifmouraroc@outlook.com>
2024-02-27 10:50:20 -0300

    ThemeToggle e menu da conta

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />{' '}
-      {/* cada página tem um título %s */}
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />{' '}
+        {/* cada página tem um título %s */}
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }

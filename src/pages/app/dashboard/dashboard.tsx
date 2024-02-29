30941ac2e84dd0337de4f342a1df97f87cf96b06
Gabriela França <gabifmouraroc@outlook.com>
2024-02-29 14:56:23 -0300

    gráfico de receita no período

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { DayOrdersAmountCard } from './day-orders-amount-card'
 import { MonthCancelsAmountCard } from './month-cancels-amount-card'
 import { MonthOrdersAmountCard } from './month-orders-amount'
 import { MonthRevenueCard } from './month-revenue-card'
+import { RevenueChart } from './revenue-chart'
 
 export function Dashboard() {
   return (
@@ -17,6 +18,10 @@ export function Dashboard() {
           <MonthOrdersAmountCard />
           <DayOrdersAmountCard />
           <MonthCancelsAmountCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
         </div>
       </div>
     </>

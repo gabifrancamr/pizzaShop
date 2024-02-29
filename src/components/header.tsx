03a7f3fe044569232c8713706eb85c0ba801ba07
Gabriela França <gabifmouraroc@outlook.com>
2024-02-29 11:50:43 -0300

    responsividade no header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,17 +8,17 @@ import { Separator } from './ui/separator'
 export function Header() {
   return (
     <div className="border-b">
-      <div className="flex h-16 items-center gap-6 px-6">
-        <Pizza className="h-6 w-6" />
-        <Separator orientation="vertical" className="h-6" />
+      <div className="flex h-16 items-center gap-6 px-8">
+        <Pizza className="hidden h-6 w-6 md:block" />
+        <Separator orientation="vertical" className="hidden h-6 md:block" />
         <nav className="flex items-center space-x-4 lg:space-x-6">
           <NavLink to="/">
             <Home className="h-4 w-4" />
-            Início
+            <span className="hidden md:block">Início</span>
           </NavLink>
           <NavLink to="orders/">
             <UtensilsCrossed className="h-4 w-4" />
-            Pedidos
+            <span className="hidden md:block">Pedidos</span>
           </NavLink>
         </nav>
 
